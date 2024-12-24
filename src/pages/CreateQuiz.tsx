00e1baccf87ee0b37545e493e50fd6bda50401f7
MrCorvoDev <mr.corvo.dev@gmail.com>
2024-12-24 11:24:04 +0400

    feat: Create CreateQuiz component

--- src/pages/CreateQuiz.tsx
+++ src/pages/CreateQuiz.tsx
@@ -1,15 +1,77 @@
+import {FieldError, FieldValues, SubmitHandler, useForm} from 'react-hook-form';
 import styled from 'styled-components';
 
+import Button from '../components/form/Button';
+import Input from '../components/form/Input';
+import Label from '../components/form/Label';
+import Section from '../components/Section';
+import {layout} from '../styles/theme';
 import em from '../styles/utils/em';
+import md from '../styles/utils/md';
 
+const HeadlineFontsize = 32;
 const Headline = styled.h1`
-   margin-top: ${em(48)};
-   font-size: ${em(32)};
+   font-size: ${em(HeadlineFontsize)};
+   font-weight: 700;
+   margin-bottom: ${em(32, HeadlineFontsize)};
 `;
 
-const CreateQuiz = () => (
-   <div className='container'>
-      <Headline>Create Quiz</Headline>
-   </div>
-);
+const Form = styled.form`
+   display: flex;
+   flex-direction: column;
+   align-items: flex-start;
+   gap: ${em(16)};
+`;
+
+const Grid = styled.div`
+   display: grid;
+   grid-template-columns: 1fr 1fr;
+   gap: ${em(16)};
+   width: 100%;
+   @media (${md(layout.md3)}) {
+      grid-template-columns: 1fr;
+   }
+`;
+
+const CreateQuiz = () => {
+   const {
+      register,
+      handleSubmit,
+      formState: {errors},
+   } = useForm();
+
+   const onSubmit: SubmitHandler<FieldValues> = async data => {
+      console.log(data);
+      await new Promise(resolve => setTimeout(resolve, 100));
+   };
+
+   return (
+      <Section>
+         <div className='container'>
+            <Headline>Create a Quiz</Headline>
+            <Form onSubmit={event => void handleSubmit(onSubmit)(event)}>
+               <Grid>
+                  <Label title='Quiz Name'>
+                     <Input
+                        name='quizName'
+                        register={register}
+                        error={errors.quizName as FieldError | undefined}
+                     />
+                  </Label>
+                  <Label title='Quiz Description'>
+                     <Input
+                        name='quizDescription'
+                        register={register}
+                        error={errors.quizName as FieldError | undefined}
+                     />
+                  </Label>
+               </Grid>
+               <Grid>
+                  <Button type='submit'>Submit</Button>
+               </Grid>
+            </Form>
+         </div>
+      </Section>
+   );
+};
 export default CreateQuiz;
