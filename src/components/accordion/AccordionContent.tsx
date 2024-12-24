f73193cdddfefe9934da903cb10011bb13630358
MrCorvoDev <mr.corvo.dev@gmail.com>
2024-12-24 18:15:19 +0400

    fix(AccordionContent): Change animation

--- src/components/accordion/AccordionContent.tsx
+++ src/components/accordion/AccordionContent.tsx
@@ -6,11 +6,19 @@ import {useAccordion} from './Accordion';
 const AccordionContent = ({children}: ReactPropsChildrenType) => {
    const {isOpened} = useAccordion();
    const animation = isOpened
-      ? {overflow: 'auto', opacity: 1, height: 'auto'}
+      ? {overflow: 'visible', opacity: 1, height: 'auto'}
       : {overflow: 'hidden', opacity: 0, height: 0};
 
    return (
-      <motion.div initial={animation} animate={animation}>
+      <motion.div
+         initial={animation}
+         animate={animation}
+         transition={{
+            type: 'spring',
+            duration: 0.5,
+            bounce: 0.3,
+         }}
+      >
          {children}
       </motion.div>
    );
